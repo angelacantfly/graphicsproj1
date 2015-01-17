78a14e5f5afc00fc97aa5c8ded9d01ea6874c402
Z Sweedyk <zsweedyk@Elizabeths-MacBook-Pro.local>
2015-01-17 07:44:47 -0800

    Initial Commit

---
+++ ip2015/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ip2015
+//
+//  Created by Z Sweedyk on 1/17/15.
+//  Copyright (c) 2015 Z Sweedyk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
